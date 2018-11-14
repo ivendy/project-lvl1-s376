46a0fce8e448750afda4ca94a87633aec2f6617c
ivendy <ivendy@gmail.com>
2018-11-14 16:07:10 +0300

    add function from step 2 of the project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const readlineSync = require('readline-sync');
 
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hi ${userName}!`);
+export default () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hi ${userName}!`);
+};
