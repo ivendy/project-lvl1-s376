ff0f7a2f4f801e2c3363f6d14316d0318384ab95
ivendy <ivendy@gmail.com>
2018-11-14 12:08:06 +0300

    it will show you 134 error on make test

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ install:
 start:
 	npx babel-node -- src/bin/brain-games.js
 
+test:
+	npx babel-node -- src/index.js
+
 publish:
 	npm publish
---
+++ src/index.js
@@ -0,0 +1,4 @@
+var readlineSync = require('readline-sync');
+
+let userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
