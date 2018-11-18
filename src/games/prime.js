4aef3f34378e5e2c1f153e8d9a50246174de966e
ivendy <ivendy@gmail.com>
2018-11-18 11:59:32 +0300

    minor changes

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,6 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (number) => {
   if (number <= 1) return false;
-  if (number === 2) return true; // почему избыточная проверка? без нее же false возвращает
   for (let i = 2; i * i < number; i += 1) {
     if (number % i === 0) return false;
   }
@@ -14,7 +13,7 @@ const isPrime = (number) => {
 
 const makeRound = () => {
   const question = generateNum(2, 1000);
-  const answer = isPrime(question) ? 'yes' : 'no';
+  const answer = isPrime(question) ? 'yes' : 'no';  
   return [question, answer];
 };
 
